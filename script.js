ba7ad065185b47765daf2215b16c32af7a537a5d
arthur <arthurazevedods@gmail.com>
2024-03-23 20:09:29 -0300

    new elements and btn functions

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 let currentIndex = 0; // Índice da palavra atual
+let btn = false;
+let knowWords = 0;
+const flashWord = document.querySelector(".word");
+const flashTraducao = document.querySelector(".traducao");
+const numberWords = document.querySelector(".numberWords");
+const conhecidas = document.querySelector(".conhecidas");
+const buttonKnow = document.getElementById("btnKnow");
+const buttonDontKnow = document.getElementById("btnDontKnow");
 
-function updateWord() {
-    const flashWord = document.querySelector(".word");
-    const flashTraducao = document.querySelector(".traducao");
 
+function updateWord(btn) {
     // Verifica se as palavras já foram carregadas
     if (window.jsonWords && window.jsonWords.length > 0) {
+        if(btn == true) {
+            knowWords++;
+        }
+        //Atualiza as informações
+        numberWords.innerHTML = `${currentIndex}/${window.jsonWords.length}`
+        conhecidas.innerHTML = `${knowWords}`;
         // Exibe a palavra atual
         flashWord.innerHTML = window.jsonWords[currentIndex]["word"].toUpperCase();
         flashTraducao.innerHTML = window.jsonWords[currentIndex]["traducao"].toUpperCase();
-
+        
         // Atualiza o índice para a próxima palavra (lista circular)
         currentIndex = (currentIndex + 1) % window.jsonWords.length;
+        if(currentIndex == 0){
+            knowWords = -1;
+        }
     } else {
         console.error('Erro: Nenhuma palavra disponível.');
     }
@@ -25,9 +40,8 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             // Faça algo com os dados carregados, por exemplo, atribua-os a uma variável global
             window.jsonWords = data;
-
             // Atualiza a palavra assim que os dados forem carregados
-            updateWord();
+            updateWord(false);
         })
         .catch(error => {
             console.error('Erro ao carregar palavras:', error);
@@ -35,4 +49,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Adiciona um ouvinte de evento para o botão 'changeButton'
-document.getElementById('changeButton').addEventListener('click', updateWord);
+document.getElementById('changeButton').addEventListener('click', function() {
+    updateWord(false);
+});
+
+
+
+document.getElementById('bntKnow').addEventListener('click', function() {
+    updateWord(true);
+});
